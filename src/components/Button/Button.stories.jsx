a740d5d6ad4dfbabd9e6d8b87414f250ef62e41a
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-02-27 23:07:00 +0530

    feat: button improvements

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,44 +1,50 @@
-import React from 'react';
-import { Back } from '../Icon';
-import  Button  from './Button';
+import React from "react";
+import { Back } from "../Icon";
+import Button from "./Button";
 
 export default {
-  title: 'UI/Button',
+  title: "UI/Button",
   component: Button,
 };
 const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  label: 'Button',
+  label: "Button",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  type: 'secondary',
-  label: 'Button',
+  type: "secondary",
+  label: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  size: "large",
+  label: "Button",
 };
 
 export const Medium = Template.bind({});
 Medium.args = {
-  size: 'medium',
-  label: 'Button',
+  size: "medium",
+  label: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: "small",
+  label: "Button",
 };
 
 export const Icon = Template.bind({});
 Icon.args = {
-  label: 'Button',
-  icon: <Back size={15} color="white"/>
+  label: "Button",
+  icon: <Back size={15} color="white" />,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: "Button",
+  disabled: true,
 };
